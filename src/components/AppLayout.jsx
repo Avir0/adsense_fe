fdf8da48210c104e214b32de8596bc1df58b2553
Avinash Kumar <141600535+Avir0@users.noreply.github.com>
2026-01-19 16:32:53 +0530

    Update AppLayout.jsx

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,29 +1,50 @@
 import React from "react";
+// import { Box, Flex } from "@chakra-ui/react";
+// import Sidebar from "./Sidebar";
+// import Topbar from "./Topbar";
+
+// const AppLayout = ({ children }) => {
+//   return (
+//     <Flex minH="100vh" bg="gray.100">
+      
+//       {/* Sidebar */}
+//       <Sidebar />
+
+//       {/* Main content */}
+//       <Box flex="1" display="flex" flexDirection="column">
+        
+//         {/* Topbar */}
+//         <Topbar />
+
+//         {/* Page Content */}
+//         <Box flex="1" p={{ base: 4, md: 6 }}>
+//           {children}
+//         </Box>
+
+//       </Box>
+//     </Flex>
+//   );
+// };
+
+// export default AppLayout;
+
+import React from "react";
 import { Box, Flex } from "@chakra-ui/react";
 import Sidebar from "./Sidebar";
 import Topbar from "./Topbar";
 
 const AppLayout = ({ children }) => {
   return (
-    <Flex minH="100vh" bg="gray.100">
-      
-      {/* Sidebar */}
+    <Flex minH="100vh" bg="gray.50">
       <Sidebar />
 
-      {/* Main content */}
-      <Box flex="1" display="flex" flexDirection="column">
-        
-        {/* Topbar */}
+      <Box flex="1">
         <Topbar />
-
-        {/* Page Content */}
-        <Box flex="1" p={{ base: 4, md: 6 }}>
-          {children}
-        </Box>
-
+        <Box p={{ base: 4, md: 6 }}>{children}</Box>
       </Box>
     </Flex>
   );
 };
 
 export default AppLayout;
+
