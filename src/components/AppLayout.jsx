9f09ca76f2cb8a0dbcb791c7398f6e6169f5c0bf
Avinash Kumar <141600535+Avir0@users.noreply.github.com>
2026-01-19 16:25:39 +0530

    Create AppLayout.jsx

---
+++ src/components/AppLayout.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Box, Flex } from "@chakra-ui/react";
+import Sidebar from "./Sidebar";
+import Topbar from "./Topbar";
+
+const AppLayout = ({ children }) => {
+  return (
+    <Flex minH="100vh" bg="gray.100">
+      
+      {/* Sidebar */}
+      <Sidebar />
+
+      {/* Main content */}
+      <Box flex="1" display="flex" flexDirection="column">
+        
+        {/* Topbar */}
+        <Topbar />
+
+        {/* Page Content */}
+        <Box flex="1" p={{ base: 4, md: 6 }}>
+          {children}
+        </Box>
+
+      </Box>
+    </Flex>
+  );
+};
+
+export default AppLayout;
